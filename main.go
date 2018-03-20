bb332859a82f1301b89ca0fff1687cd35fe7cfa1
Zhdan <zhdanruslan@i.ua>
2018-03-20 10:25:11 +0200

    fill slice of persons manually

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+
+	//!~~~fill slice of persons manually~~~
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+
 	router.HandleFunc("/people", GetPeople).Methods("GET")
 	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
 	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
@@ -16,13 +22,12 @@ func main() {
 }
 
 
+
 //!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 func GetPeople(w http.ResponseWriter, r *http.Request) {}
 func GetPerson(w http.ResponseWriter, r *http.Request) {}
 func CreatePerson(w http.ResponseWriter, r *http.Request) {}
 func DeletePerson(w http.ResponseWriter, r *http.Request) {}
-//!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 //!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 type Person struct {
 	ID        string   `json:"id,omitempty"`
@@ -30,10 +35,10 @@ type Person struct {
 	Lastname  string   `json:"lastname,omitempty"`
 	Address   *Address `json:"address,omitempty"`
 }
+
 type Address struct {
 	City  string `json:"city,omitempty"`
 	State string `json:"state,omitempty"`
 }
-
 var people []Person
 //!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
